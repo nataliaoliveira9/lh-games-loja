8cd87834f544dda8fc1c85b019e0e6ac40ba2f5f
Natália Oliveira <natyoliveira@proton.me>
2024-03-08 03:24:01 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'senai'`, () => {
+  it(`should have as title 'games'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('senai');
+    expect(app.title).toEqual('games');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, senai');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, games');
   });
 });
